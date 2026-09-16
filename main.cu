72a06429867ff255699526c490f6298234ab324c
Rance-21 <796388wyb@gmail.com>
2026-09-16 20:17:39 +0800

    规约求和

--- main.cu
+++ main.cu
@@ -4,7 +4,12 @@
 #include <vector>
 
 // 研究GPU矩阵转置算法
-
+/*
+只有形如 if (blockIdx.x > 0) 这种在 Block 尺度上要么全进、要么全不进的分支，
+内部放置 __syncthreads() 才是合法安全的
+单个warp内__syncthreads()导致dead lock
+Volta架构及以后可以，每个thread有单独的PC，靠SMSP的广播掩码实现，但是有L1 i-cache的开销
+*/
 #define CUDA_CHECK(call)                                                                                               \
     do {                                                                                                               \
         cudaError_t err = (call);                                                                                      \
@@ -23,7 +28,53 @@ __global__ void vector_add(const float *a, const float *b, float *c, int n) {
     }
 }
 
-int main() {
+__global__ void block_sum_kernel(const float *input, float *output, int n) {
+    extern __shared__ float s[];
+
+    int tid = threadIdx.x;
+    int i = blockIdx.x * blockDim.x + tid;
+
+    // 每个线程先把自己的数据搬进 shared memory
+    s[tid] = (i < n) ? input[i] : 0.0f;
+    // 必须等整个 block 都写完
+    __syncthreads();
+
+    for (int stride = blockDim.x / 2; stride > 0; stride /= 2) {
+        if (tid < stride) {
+            s[tid] += s[tid + stride];
+        }
+
+        // 必须等这一轮所有加法结束，
+        // 下一轮才能读取结果
+        __syncthreads();
+    }
+
+    // block 的最终结果
+    if (tid == 0) {
+        output[blockIdx.x] = s[0];
+    }
+}
+
+void block_sum(const float *h_input, float *h_output, int n) {
+    constexpr int threads = 256;
+    int blocks = (n + threads - 1) / threads;
+
+    float *d_input;
+    float *d_output;
+
+    CUDA_CHECK(cudaMalloc(&d_input, n * sizeof(float)));
+    CUDA_CHECK(cudaMemcpy(d_input, h_input, blocks * sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMalloc(&d_output, blocks * sizeof(float)));
+
+    // 第三个参数是shared_memory_bytes
+    block_sum_kernel<<<blocks, threads, threads * sizeof(float)>>>(d_input, d_output, n);
+
+    CUDA_CHECK(cudaMemcpy(h_output, d_output, blocks * sizeof(float), cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaFree(d_input));
+    CUDA_CHECK(cudaFree(d_output));
+}
+
+void vector_add() {
     // 强制编译期计算
     constexpr int N = 1 << 24;
     constexpr int THREADS = 256;
@@ -98,4 +149,30 @@ int main() {
     CUDA_CHECK(cudaFree(d_a));
     CUDA_CHECK(cudaFree(d_b));
     CUDA_CHECK(cudaFree(d_c));
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::printf("请正确传参！");
+        return 0;
+    }
+
+    char choice = *argv[1];
+    switch (choice) {
+    case 1: {
+        vector_add();
+    }
+    case 2: {
+        float *h_input;
+        for (int i = 0; i < 1024; ++i) {
+            h_input[i] = static_cast<float>(i + 1);
+        }
+        float *h_output;
+        block_sum(h_input, h_output, 1024);
+
+        for (int i = 0; i < 4; i++) {
+            std::printf("%f", h_output[i]);
+        }
+    }
+    }
 }
